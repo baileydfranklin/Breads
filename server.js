4bbced6c5445e37c8f5d1872e91afd6956d09e29
Bailey Franklin <baileydfranklin@gmail.com>
2023-03-12 11:02:37 -0700

    adding what I have

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+require('dotenv').config()
+const PORT = process.env.PORT
 
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('The PORT is over 9000!!')
+})
+
+const breadsController = require('./controllers/breads_controller')
+app.use('/breads', breadsController)
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT: ', PORT)
+})
 
